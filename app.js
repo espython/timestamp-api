61855280324d4080e87d87adb6a317c4718712f5
espython <espython85@gmail.com>
2017-01-10 12:41:34 +0200

    tidying up

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 
 app.get('/:query', function(req, res) {
     var date = req.params.query;
-    console.log(!isNaN(Date.parse(date)));
+
     if (!isNaN(Date.parse(date))) {
         var dateTime1 = new Date(date).toDateString();
-        console.log(dateTime1);
+
         var dateTime2 = Date.parse(date);
-        console.log('success')
-        console.log(dateTime2);
+
         data = { "unix": dateTime2, "natural": dateTime1 }
         res.send(data);
     } else {
         var timestamp = parseInt(date);
         var unixtime = new Date(timestamp);
         var unixdate1 = new Date(unixtime).toDateString();
-        console.log('fail')
-        console.log("unixdate", unixdate1);
-        console.log("unixTime", timestamp);
+
         datas = { "unix": timestamp, "natural": unixdate1 }
         res.send(datas);
     }
